27ed8e8d14903eeaca90f53a5cb6a0fea903d971
emabimusik <emabi2015@gmail.com>
2018-03-21 16:57:09 +0100

    First commit

---
+++ custom.js
@@ -0,0 +1,44 @@
+$(function () {
+
+    function initMap() {
+
+        var location = new google.maps.LatLng(55.75,12.55);
+
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: location,
+            zoom: 16,
+            panControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+        var markerImage = 'marker.png';
+
+        var marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            icon: markerImage
+        });
+
+        var contentString = '<div class="info-window">' +
+                '<h3>Info Car rental</h3>' +
+                '<div class="info-content">' +
+                '<p>Pellet mortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>' +
+                '</div>' +
+                '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString,
+            maxWidth: 400
+        });
+
+        marker.addListener('click', function () {
+            infowindow.open(map, marker);
+        });
+
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+});
